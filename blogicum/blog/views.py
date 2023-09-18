0500e8c1bb672ead09fe599e970e7f574ab777a4
Aleksey Vasilev <alexey.vasiljev@gmail.com>
2023-09-18 19:53:42 +0500

    After first review

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -54,7 +54,7 @@ class ProfileDetailView(GetPostMixin, DetailView):
         context['user'] = self.request.user
         context['profile'] = self.object
         context['page_obj'] = self.obj_paginator(
-            post_list.order_by('pub_date'))
+            post_list.order_by('-pub_date'))
         return context
 
 
@@ -64,11 +64,6 @@ class ProfileUpdateView(LoginRequiredMixin, GetPostMixin, UpdateView):
 
     def get_object(self):
         return self.request.user
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['profile'] = self.object
-        return context
 
     def get_success_url(self):
         return reverse('blog:profile', kwargs={'username': self.request.user})
@@ -96,9 +91,12 @@ class CategoryDetailView(GetPostMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        if not self.object.is_published:
+            raise Http404('Категория снята с публикации')
         post_list = self.post_annotated(self.object.posts).filter(
             is_published=True,
-            pub_date__lte=now())
+            pub_date__lte=now(),
+            category__is_published=True)
         context['category'] = self.object
         context['page_obj'] = self.obj_paginator(
             post_list.order_by('-pub_date'))
@@ -115,7 +113,7 @@ class PostDetailView(GetPostMixin, DetailView):
             (self.object.author != self.request.user) and
             (not self.object.is_published)
         ):
-            raise Http404
+            raise Http404('Пост снят с публикации')
         context = super().get_context_data(**kwargs)
         context['form'] = CommentForm()
         context['page_obj'] = (
@@ -139,9 +137,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class AuthorPassMixin(UserPassesTestMixin):
     def test_func(self):
-        self.current_obj = self.get_object()
-        print(self.current_obj)
-        return (self.current_obj.author == self.request.user)
+        return (self.get_object().author == self.request.user)
 
 
 class PostMixin(AuthorPassMixin):
@@ -149,13 +145,13 @@ class PostMixin(AuthorPassMixin):
     template_name = 'blog/create.html'
     pk_url_kwarg = 'post_id'
 
-    def handle_no_permission(self):
-        return redirect('blog:post_detail', post_id=self.current_obj.pk)
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['form'] = PostForm(instance=self.object)
         return context
+
+    def handle_no_permission(self):
+        return redirect('blog:post_detail', post_id=self.kwargs['post_id'])
 
 
 class PostUpdateView(LoginRequiredMixin, PostMixin, UpdateView):
@@ -170,46 +166,42 @@ class PostDeleteView(LoginRequiredMixin, PostMixin, DeleteView):
         return reverse('blog:profile', kwargs={'username': self.request.user})
 
 
-class CommentCreateView(LoginRequiredMixin, CreateView):
-    comment_post = None
-    model = Comment
-    template_name = 'blog/comment.html'
-    form_class = CommentForm
-
-    def dispatch(self, request, *args, **kwargs):
-        self.comment_post = get_object_or_404(Post, id=kwargs['post_id'])
-        return super().dispatch(request, *args, **kwargs)
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        form.instance.post = self.comment_post
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse('blog:post_detail',
-                       kwargs={'post_id': self.comment_post.pk})
-
-
 class CommentMixin(AuthorPassMixin):
     model = Comment
     pk_url_kwarg = 'comment_id'
     template_name = 'blog/comment.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = CommentForm(instance=self.object)
-        return context
 
     def get_success_url(self):
         return reverse('blog:post_detail',
                        kwargs={'post_id': self.object.post.pk})
 
     def handle_no_permission(self):
-        return redirect('blog:post_detail', post_id=self.current_obj.post.pk)
+        return redirect('blog:post_detail', post_id=self.kwargs['post_id'])
+
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    template_name = 'blog/comment.html'
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        form.instance.author = self.request.user
+        form.instance.post = post
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:post_detail',
+                       kwargs={'post_id': self.object.post.pk})
 
 
 class CommentUpdateView(LoginRequiredMixin, CommentMixin, UpdateView):
     form_class = CommentForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm(instance=self.object)
+        return context
 
 
 class CommentDeleteView(LoginRequiredMixin, CommentMixin, DeleteView):
